51c1cf4d07c97821b5842200fa92a1a1d4d780df
Kiichi Hamanaka <b1695205@planet.kanazawa-it.ac.jp>
2022-07-14 08:32:41 +0900

    登録処理の分離

--- src/lib/utils/spotify.ts
+++ src/lib/utils/spotify.ts
@@ -1,8 +1,10 @@
 import { SpotifyWebApi } from "spotify-web-api-ts";
 import { AuthorizationScope } from "spotify-web-api-ts/types/types/SpotifyAuthorization";
+import {prisma} from "./prisma";
 
 const scopes: AuthorizationScope[] = [
   "user-top-read",
+  "user-read-private",
 ];
 const state = "spotify-top-collector";
 
@@ -16,3 +18,67 @@ export const authorizeURL = spotifyApi.getRefreshableAuthorizationUrl({
   scope: scopes,
   state,
 });
+
+export const registrationTop = async (accessToken:string) =>{
+  spotifyApi.setAccessToken(accessToken)
+  const user = await spotifyApi.users.getMe()
+  const name = user.display_name || 'noname'
+  const topTracks = await spotifyApi.personalization.getMyTopTracks({limit: 10})
+  const topArtists = await spotifyApi.personalization.getMyTopArtists({limit: 10})
+
+  const upsertUser = await prisma.user.upsert({
+    create: {
+      displayName: name,
+      uri: user.uri,
+    },
+    update: {
+      displayName: name,
+    },
+    where: {
+      uri: user.uri
+    },
+  })
+
+  const createTracks = await prisma.$transaction(
+      topTracks.items.map((item) => {
+            return prisma.track.upsert({
+              create: {uri:item.uri},
+              update: {},
+              where: {uri: item.uri},
+            })
+          }
+      )
+  )
+
+  const createArtists = await prisma.$transaction(
+      topArtists.items.map((item) => {
+            return prisma.artist.upsert({
+              create: {uri:item.uri},
+              update: {},
+              where: {uri: item.uri},
+            })
+          }
+      )
+  )
+
+  const createUserTopTrack = await prisma.userTopTrack.createMany({
+    data: createTracks.map((item) => {
+      return {
+        userId: upsertUser.id,
+        trackId: item.id,
+      }
+    }),
+    skipDuplicates: true,
+  });
+
+  const createUserTopArtist = await prisma.userTopArtist.createMany({
+    data: createArtists.map((item) => {
+      return {
+        userId: upsertUser.id,
+        artistId: item.id,
+      }
+    }),
+    skipDuplicates: true,
+  });
+  console.log('end')
+}
--- src/pages/api/regist.ts
+++ src/pages/api/regist.ts
@@ -1,6 +1,6 @@
-import {spotifyApi} from "../../lib/utils/spotify";
+import {registrationTop} from "../../lib/utils/spotify";
 import {NextApiRequest, NextApiResponse} from "next";
-import {prisma} from "../../lib/utils/prisma";
+import {withSessionRoute} from "../../lib/withSession";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const {
@@ -9,73 +9,14 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     switch (method) {
         case "PUT": {
             try {
-                const user = await spotifyApi.users.getMe()
-                const name = user.display_name || 'noname'
-
-                const topTracks = await spotifyApi.personalization.getMyTopTracks({limit: 10})
-                const topArtists = await spotifyApi.personalization.getMyTopArtists({limit: 10})
-
-                const upsertUser = await prisma.user.upsert({
-                    create: {
-                        displayName: name,
-                        uri: user.uri,
-                    },
-                    update: {
-                        displayName: name,
-                    },
-                    where: {
-                        uri: user.uri
-                    },
-                })
-
-                const createTracks = await prisma.$transaction(
-                    topTracks.items.map((item) => {
-                            return prisma.track.upsert({
-                                create: {uri:item.uri},
-                                update: {},
-                                where: {uri: item.uri},
-                            })
-                        }
-                    )
-                )
-
-                const createArtists = await prisma.$transaction(
-                    topArtists.items.map((item) => {
-                            return prisma.artist.upsert({
-                                create: {uri:item.uri},
-                                update: {},
-                                where: {uri: item.uri},
-                            })
-                        }
-                    )
-                )
-
-                const createUserTopTrack = await prisma.userTopTrack.createMany({
-                    data: createTracks.map((item) => {
-                        return {
-                            userId: upsertUser.id,
-                            trackId: item.id,
-                        }
-                    }),
-                    skipDuplicates: true,
-                });
-
-                const createUserTopArtist = await prisma.userTopArtist.createMany({
-                    data: createArtists.map((item) => {
-                        return {
-                            userId: upsertUser.id,
-                            artistId: item.id,
-                        }
-                    }),
-                    skipDuplicates: true,
-                });
-
-                console.dir(createUserTopTrack, {depth:null})
-                console.dir(createUserTopArtist, {depth:null})
-
-                res.status(200).redirect("/");
+                if(req.session.accessToken){
+                    await registrationTop(req.session.accessToken);
+                }
+                res.status(200).end();
                 break;
             } catch (e) {
+                console.log(e)
+                res.status(400).end();
                 break;
             }
         }
@@ -85,4 +26,4 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     }
 };
 
-export default handler;
+export default withSessionRoute(handler);
